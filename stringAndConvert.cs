9978272790fa29f932957d9a5485a429a9fbdde1
Onur Şimşek <38193815+simsekonur@users.noreply.github.com>
2020-08-20 16:03:13 +0300

    Create stringAndConvert.cs

---
+++ stringAndConvert.cs
@@ -0,0 +1,17 @@
+using System;
+class Program {
+    static void Main(string[] args){
+       
+
+        Console.Write("Enter a color:");
+        string color = Console.ReadLine();
+        Console.Write("Enter the age:");
+        string age = Console.ReadLine();
+        Console.WriteLine("This is the color:"+color);
+        Console.WriteLine("This is the age:"+ age);
+        
+        
+        
+    }
+
+}
