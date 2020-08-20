e235c9aaa2f67f4b5775f0721fe3acdb49cdeea4
Onur Şimşek <38193815+simsekonur@users.noreply.github.com>
2020-08-20 16:16:56 +0300

    Create arrray.cs

---
+++ arrray.cs
@@ -0,0 +1,18 @@
+
+using System;
+class Program {
+    static void Main(string[] args){
+       
+    int [] luckNumbers = {4,5,10,9,11,12};
+    Console.WriteLine(luckNumbers[0]);
+    
+    luckNumbers[0] = 0 ;
+    Console.WriteLine(luckNumbers[0]);
+    string [] friends = new string[3];/*how many element must be said*/
+    
+    
+    
+        
+    }
+
+}
