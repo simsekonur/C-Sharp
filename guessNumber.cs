e0a1156638a55474c7d5dbf733034a007756fcae
Onur Şimşek <38193815+simsekonur@users.noreply.github.com>
2020-08-21 09:36:24 +0300

    Update guessNumber.cs

--- guessNumber.cs
+++ guessNumber.cs
@@ -6,18 +6,28 @@ class program {
         int secretNumber = 32 ;
         
         int guess = -1;
+        int remainingHealth= 3 ;
+        
         
         while(guess!= secretNumber){
             Console.Write("Enter the guess:");
             guess = Convert.ToInt32(Console.ReadLine());
-            if (guess < secretNumber){
-                Console.WriteLine("Guess is smaller,say higher..");
+            remainingHealth--;
+            if (remainingHealth==0){
+                Console.WriteLine("You lost the game..");
+                break ;
             }
-            else {
+            else if (guess < secretNumber){
+                Console.WriteLine("Say higher..");
+            }
+            else if (guess > secretNumber){
                 Console.WriteLine("Say small number");
             }
+            else {
+                Console.WriteLine("You guessed correctly!");
+            }
         }
-        Console.WriteLine("You guessed correctly!");
+       
         
     }
 }
