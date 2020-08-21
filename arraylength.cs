78262f61744a8f524f154adb35d465be8573df42
Onur Şimşek <38193815+simsekonur@users.noreply.github.com>
2020-08-21 09:47:46 +0300

    Create arraylength.cs

---
+++ arraylength.cs
@@ -0,0 +1,13 @@
+using System ;
+
+class program {
+    static void Main (string []args){
+       
+        int [] luckynumbers = {1,2,3,4,5};    
+        for (int i = 0 ; i< luckynumbers.Length;i++){
+            Console.WriteLine(luckynumbers[i]);
+        }
+       
+        
+    }
+}
