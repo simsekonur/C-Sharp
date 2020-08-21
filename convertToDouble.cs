4a8f5640ddbab9fe13232fb0de1f030d3308128d
Onur Şimşek <38193815+simsekonur@users.noreply.github.com>
2020-08-21 08:24:45 +0300

    Update and rename convertToInteger.cs to convertToDouble.cs

--- convertToDouble.cs
+++ convertToDouble.cs
@@ -3,9 +3,9 @@ using System;
 class Program {
     static void Main(string [] args){
         Console.Write("Enter the first number:");
-        int num1 = Convert.ToInt32(Console.ReadLine());
+        double num1 = Convert.ToDouble(Console.ReadLine());
         Console.Write("Enter the second number:");
-        int num2 = Convert.ToInt32(Console.ReadLine());
+        double num2 = Convert.ToDouble(Console.ReadLine());
         //whathever it comes from console.readline will
         //be converted into an integer
         Console.WriteLine(num1+num2);
